732de1a84e4d78e9570c9ba7bf58c35fcbdefdf4
Amon Wiechmann <ajwiechmann@web.de>
2026-01-29 10:12:47 +0100

    Added Highlight to Navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,25 +1,32 @@
-import { Nav, Navbar, NavDropdown, Container } from "react-bootstrap";
+import { Nav, Navbar, Container } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 import "./css/Header.css";
 
 function Header() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="/" className="d-flex align-items-center gap-2">
-          <img
-            src="/images/logo.png"
-            alt="x-min-city logo"
-            height="28"
-            className="d-inline-block align-top"
-          />
+        <Navbar.Brand
+          as={NavLink}
+          to="/"
+          className="d-flex align-items-center gap-2"
+        >
+          <img src="/images/logo.png" alt="x-min-city logo" height="28" />
           <span>x-minute-city</span>
         </Navbar.Brand>
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/reachmap">ReachMap</Nav.Link>
-            <Nav.Link href="/cityscope">CityScope</Nav.Link>
-            <Nav.Link href="/impressum">Impressum</Nav.Link>
+            <Nav.Link as={NavLink} to="/reachmap">
+              ReachMap
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/cityscope">
+              CityScope
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/impressum">
+              Impressum
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
